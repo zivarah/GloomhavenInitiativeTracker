64c6ed4fe63f2c79c49f6c3a06567a7148bcc2c9
Brian Lyles <1123282+zivarah@users.noreply.github.com>
2022-05-14 00:08:51 -0500

    Add remaining character icons and fix sizing

--- src/model/Character.ts
+++ src/model/Character.ts
@@ -11,14 +11,23 @@ export function isCharacter(trackableClass: ITrackableClass): trackableClass is 
 }
 
 export enum CharacterClass {
-	mindthief = 1,
-	scoundrel,
-	spellweaver,
-	cragheart,
+	beastTyrant = 1,
+	berserker,
 	brute,
-	tinkerer,
+	cragheart,
 	doomstalker,
+	elementalist,
+	mindthief,
+	nightshroud,
 	plagueherald,
+	quartermaster,
+	sawbones,
+	scoundrel,
+	soothsinger,
+	spellweaver,
+	summoner,
+	sunkeeper,
+	tinkerer,
 }
 
 export interface ICharacterClassInfo extends ITrackableClassInfo {
@@ -29,44 +38,93 @@ export interface ICharacterClassInfo extends ITrackableClassInfo {
 const iconPath = process.env.PUBLIC_URL + "/images/characters/";
 
 const characterClassInfos: { [classId in CharacterClass]: ICharacterClassInfo } = {
-	[CharacterClass.mindthief]: {
-		name: "Mindthief",
-		iconKey: "mindthief.png",
+	[CharacterClass.beastTyrant]: {
+		name: "Beast Tyrant",
+		iconKey: "beastTyrant.png",
+		spoilerAlias: "Two Minis",
 	},
-	[CharacterClass.scoundrel]: {
-		name: "Scoundrel",
-		iconKey: "scoundrel.png",
-	},
-	[CharacterClass.spellweaver]: {
-		name: "Spellweaver",
-		iconKey: "spellweaver.png",
-	},
-	[CharacterClass.cragheart]: {
-		name: "Cragheart",
-		iconKey: "cragheart.png",
+	[CharacterClass.berserker]: {
+		name: "Berserker",
+		iconKey: "berserker.png",
+		spoilerAlias: "Lightning Bolt",
 	},
 	[CharacterClass.brute]: {
 		name: "Brute",
 		iconKey: "brute.png",
 	},
-	[CharacterClass.tinkerer]: {
-		name: "Tinkerer",
-		iconKey: "tinkerer.png",
+	[CharacterClass.cragheart]: {
+		name: "Cragheart",
+		iconKey: "cragheart.png",
 	},
 	[CharacterClass.doomstalker]: {
 		name: "Doomstalker",
 		iconKey: "doomstalker.png",
 		spoilerAlias: "Angry Face",
 	},
+	[CharacterClass.elementalist]: {
+		name: "Elementalist",
+		iconKey: "elementalist.png",
+		spoilerAlias: "Triforce",
+	},
+	[CharacterClass.mindthief]: {
+		name: "Mindthief",
+		iconKey: "mindthief.png",
+	},
+	[CharacterClass.nightshroud]: {
+		name: "Nightshroud",
+		iconKey: "nightshroud.png",
+		spoilerAlias: "Eclipse",
+	},
 	[CharacterClass.plagueherald]: {
 		name: "Plagueherald",
 		iconKey: "plagueherald.png",
 		spoilerAlias: "Cthulu",
 	},
+	[CharacterClass.quartermaster]: {
+		name: "Quartermaster",
+		iconKey: "quartermaster.png",
+		spoilerAlias: "Three Spears",
+	},
+	[CharacterClass.sawbones]: {
+		name: "Sawbones",
+		iconKey: "sawbones.png",
+		spoilerAlias: "Saw",
+	},
+	[CharacterClass.scoundrel]: {
+		name: "Scoundrel",
+		iconKey: "scoundrel.png",
+	},
+	[CharacterClass.soothsinger]: {
+		name: "Soothsinger",
+		iconKey: "soothsinger.png",
+		spoilerAlias: "Music Note",
+	},
+	[CharacterClass.spellweaver]: {
+		name: "Spellweaver",
+		iconKey: "spellweaver.png",
+	},
+	[CharacterClass.summoner]: {
+		name: "Summoner",
+		iconKey: "summoner.png",
+		spoilerAlias: "Circles",
+	},
+	[CharacterClass.sunkeeper]: {
+		name: "Sunkeeper",
+		iconKey: "sunkeeper.png",
+		spoilerAlias: "Sun",
+	},
+	[CharacterClass.tinkerer]: {
+		name: "Tinkerer",
+		iconKey: "tinkerer.png",
+	},
 };
 
-// To convert all webp icons into trimmed, resized PNGs:
-// mogrify -format png -trim -resize 48x48 *.webp
+// To trim to the correct 48x48 size:
+// convert -trim -background white -gravity center -resize 48x48 -extent 48x48 berserker.png berserker.png
+// To remove transparency:
+// convert sunkeeper.png -background white -flatten sunkeeper.png
+// To fully black & white something:
+// convert doomstalker.png -threshold 50% doomstalker.png
 
 export function getCharacterClassName(classId: CharacterClass): string {
 	return characterClassInfos[classId].name;
